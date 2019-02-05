30b3e79e609e17cef39330255498c11411d1d102
Cara Eppes <caraeppes@zipcoders-MacBook-Pro-4.local>
2019-02-05 13:06:32 -0500

    asks user to input name and prints message based on input

--- Main.java
+++ Main.java
@@ -1,9 +1,30 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
 
-    }
-}
+       Scanner in = new Scanner(System.in);
+       
+       System.out.println("Enter your name:");
+       
+       String name = in.nextLine();
+       
+       if (name.equals("Bob")){
+           System.out.println("Hello Bob!");
+        }
+        
+       else if (name.equals("Alice")){
+            System.out.println("Yoooo Alice!");
+        }
+        
+        else {
+            System.out.println("Byeeeee.");
+        }
+    }  
+   }
+
